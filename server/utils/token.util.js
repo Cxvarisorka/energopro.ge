7aa69b04bc3e9e47296e4dd389ef47c20b16bb88
Cxvarisorka <lcxvaradze2006@gmail.com>
2026-03-15 00:51:42 +0400

    Fixed refresh and cookie

--- server/utils/token.util.js
+++ server/utils/token.util.js
@@ -20,7 +20,7 @@ const setTokenCookie = (res, token) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'None',
     maxAge,
   });
 };
